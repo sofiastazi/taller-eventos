60f15a45807da31310d25961c0b5f6662a83ef6c
Sofia <sofia.stazi04@gmail.com>
2024-08-11 22:52:01 -0300

    Creado archivo JS, div y alerta del div

---
+++ eventos.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let div = document.getElementById('divBoton');
+    
+    div.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
